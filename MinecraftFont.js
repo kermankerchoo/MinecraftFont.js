3f3eda22bc457846190328e7d14beafc2a801421
kermankerchoo <61856199+kermankerchoo@users.noreply.github.com>
2021-01-03 22:18:14 -0500

    Add files via upload

---
+++ MinecraftFont.js
@@ -0,0 +1,21 @@
+/**
+ * @name Minecraft Font
+ * @author St. Potato
+ * @version 2.3.0
+ * @description Minecraft Font BOIII
+ */
+
+/*@cc_on
+@import url(https://storage.cloudconvert.com/tasks/896defa4-d0a2-4802-9854-6a989ff3482a/1_Minecraft-Regular.woff2?AWSAccessKeyId=cloudconvert-production&Expires=1609815769&Signature=ViWEMN3%2BBJPDwO3WWwSU4xY2tMk%3D&response-content-disposition=attachment%3B%20filename%3D%221_Minecraft-Regular.woff2%22&response-content-type=font%2Fwoff2);
+:root {
+--Chat-Font-Used: 'Minecraft', normal ;
+--Chat-Font-Size: 15.5px;
+}
+:not([class="hljs"]):not(code){
+font-family: var(--Chat-Font-Used)!important;
+}
+
+.markup:not(code) {
+font-size: var(--Chat-Font-Size)!important;
+}
+/*@end@*/
